211cfb907cb97071e700095acca0b27aa6e769de
Justin Lake <lake.justin.m@gmail.com>
2021-01-21 08:13:24 -0600

    feat :: Update App.js with components and state management

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-const App = () => (
-    <div className="App">
-        Hello World
-    </div>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from './utils/fetchUtils';
+import Header from './components/Header';
+import Post from './components/Post';
+
+const App = () => {
+    const posts = useSelector((state) => state.blogReducer.postList);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPosts({ _page: 1 }));
+    }, [dispatch]);
+
+    return (
+        <div className="app">
+            <Header />
+            { posts && posts.map((post) => (
+                <Post
+                    body={ post.body }
+                    id={ post.id }
+                    key={ post.id }
+                    title={ post.title }
+                    userId={ post.userId }
+                />
+            )) }
+        </div>
+    );
+};
 
 export default App;
